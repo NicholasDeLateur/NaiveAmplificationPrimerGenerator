52c6163206b7e1e119c4c5cc890f7ca017537766
NicholasDeLateur <delateur@mit.edu>
2016-05-18 02:59:31 -0400

    Version 1.1, added reading from an excel

--- NaiveAmplificationPrimerGenerator.py
+++ NaiveAmplificationPrimerGenerator.py
@@ -1,9 +1,19 @@
 # written Nicholas A DeLateur in Weiss Lab, MIT; delateur@mit.edu
-# Version 1.0
+# Version 1.1
 # Run using PyCharm Community Edition 2016.1.3 with Anaconda3 on Windows 10
 
-#import xlsxwriter
-#import datetime
+import xlrd
+import xlsxwriter
+import datetime
+import os
+from itertools import takewhile
+
+def column_len(sheet, index):
+    col_values = sheet.col_values(index)
+    col_len = len(col_values)
+    for _ in takewhile(lambda x: not x, reversed(col_values)):
+        col_len -= 1
+    return col_len
 
 def RC_DNA(sequence):
     # Taken from https://gist.github.com/crazyhottommy/7255638#file-reverse_complement-py
@@ -20,7 +30,7 @@ def CalculateTm(sequence):
     return Tm
 
 
-def CreatePrimer(sequence):
+def CreatePrimers(sequence):
     primer5to3 = ''
     primer3to5 = ''
     sequenceRC = RC_DNA(sequence)
@@ -32,7 +42,6 @@ def CreatePrimer(sequence):
         i += 1
         if i >= 40 or i == len(sequence):
             break
-    print(Tm, ' ', i)
     Tm = 0
     i = 0
     while (Tm < 68):
@@ -41,31 +50,45 @@ def CreatePrimer(sequence):
         i += 1
         if i >= 40 or i == len(sequence):
             break
-    print(Tm, ' ', i)
     primers = [primer5to3,primer3to5]
     return primers
 
 
-Sequences_of_Interest = ['ATGACAGTAAAGAAGCTTTATTTCGTCCCAGCAGGTCGTTGTATGTTGGATCATTCGTCTGTTAATAGTACATTAACACCAGGAGAATTATTAGACTTACCGGTTTGGTGTTATCTTTTGGAGACTGAAGAAGGACCTATTTTAGTAGATACAGGTATGCCAGAAAGTGCAGTTAATAATGAAGGTCTTTTTAACGGTACATTTGTCGAAGGGCAGGTTTTACCGAAAATGACTGAAGAAGATAGAATCGTGAATATTTTAAAACGGGTTGGTTATGAGCCGGAGGACCTTCTTTATATTATTAGTTCTCACTTGCATTTTGATCATGCAGGAGGAAATGGCGCTTTTATAAATACACCAATCATTGTACAGCGTGCTGAATATGAGGCGGCGCAGCATAGCGAAGAATATTTGAAAGAATGTATATTGCCGAATTTAAACTACAAAATCATTGAAGGTGATTATGAAGTCGTACCAGGAGTTCAATTATTGCATACACCAGGCCATACTCCAGGGCATCAATCGCTATTAATTGAGACAGAAAAATCCGGTCCTGTATTATTAACGATTGATGCATCGTATACGAAAGAGAATTTTGAAAATGAAGTGCCATTTGCGGGATTTGATTCAGAATTAGCTTTATCTTCAATTAAACGTTTAAAAGAAGTGGTGATGAAAGAGAAGCCGATTGTTTTCTTTGGACATGATATAGAGCAGGAAAGGGGATGTAAAGTGTTCCCTGAATATATATAA',
-]
-Primers = [CreatePrimer(s) for s in Sequences_of_Interest]
+Sequences_of_Interest = []
+Primers = []
+
+f = os.path.normpath("C:/Users/Weiss Lab/Desktop/a.xlsx")
+workbookRead = xlrd.open_workbook(f)
+worksheetRead = workbookRead.sheet_by_index(0)
+
+for i in range(1, column_len(worksheetRead,0)):
+    Sequences_of_Interest.append(worksheetRead.row_values(i))
+print(Sequences_of_Interest)
+
+for i in range (len(Sequences_of_Interest)):
+    Primers.append(CreatePrimers(str(Sequences_of_Interest[i][0])))
 print(Primers)
-
-
 # Create an new Excel file and add a worksheet.
 # WARNING doesn't check if the filename already exists.
 # Shouldn't be a problem if you don't run the program within seconds of itself
-#filename = 'AmplicationPrimers' + datetime.datetime.now().strftime("%y%m%d%H%M%S") + '.xlsx'
-#workbook1 = xlsxwriter.Workbook(filename)
-#worksheet1 = workbook1.add_worksheet()
-#worksheet1.write(0, 0, 'Name')
-#worksheet1.write(0, 1, '5to3 primer')
-#worksheet1.write(0, 2, 'Tm')
-#worksheet1.write(0, 3, '3to5 primer')
-#worksheet1.write(0, 4, 'Tm')
-#worksheet1.write(0, 5, 'Sequence')
-#for i in range(0,M):
-#    worksheet1.write(i+1, 0, GeneratedSequences[i])
+# (ROW, COLUMN)....(LEFT, TOP)
+filename = 'AmplicationPrimers' + datetime.datetime.now().strftime("%y%m%d%H%M%S") + '.xlsx'
+workbook1 = xlsxwriter.Workbook(filename)
+worksheet1 = workbook1.add_worksheet()
+worksheet1.write(0, 1, 'Name')
+worksheet1.write(0, 2, '5to3 primer')
+worksheet1.write(0, 3, 'Tm')
+worksheet1.write(0, 4, '3to5 primer')
+worksheet1.write(0, 5, 'Tm')
+worksheet1.write(0, 0, 'Sequence')
+
+for i in range(len(Sequences_of_Interest)):
+    worksheet1.write(i + 1, 0, Sequences_of_Interest[i][0])
+    worksheet1.write(i + 1, 1, Sequences_of_Interest[i][1])
+    worksheet1.write(i + 1, 2, Primers[i][0])
+    worksheet1.write(i + 1, 3, CalculateTm(Primers[i][0]))
+    worksheet1.write(i + 1, 4, Primers[i][1])
+    worksheet1.write(i + 1, 5, CalculateTm(Primers[i][1]))
 
 # MANDATORY close the workbook
-#workbook1.close()
+workbook1.close()
